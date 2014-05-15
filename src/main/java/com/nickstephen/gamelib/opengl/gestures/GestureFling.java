11c07cbe32930373c26c2575966766d714229b02
Nicholas Stephen <git@nickstephen.com>
2014-05-16 02:47:06 +1000

    Moved Direction enum to the lib

--- src/main/java/com/nickstephen/gamelib/opengl/gestures/GestureFling.java
+++ src/main/java/com/nickstephen/gamelib/opengl/gestures/GestureFling.java
@@ -2,6 +2,8 @@ package com.nickstephen.gamelib.opengl.gestures;
 
 import android.view.MotionEvent;
 
+import com.nickstephen.gamelib.util.Direction;
+
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -11,10 +13,25 @@ public class GestureFling extends GestureEvent {
     public final float xVelocity;
     public final float yVelocity;
 
+    public final Direction direction;
+
     GestureFling(@NotNull MotionEvent e, float xVel, float yVel) {
         super(e, Type.FLING);
 
         xVelocity = xVel;
         yVelocity = -yVel;
+
+        float absX = Math.abs(xVelocity);
+        float absY = Math.abs(yVelocity);
+
+        if (yVelocity > 0 && absY > absX) {
+            direction = Direction.DOWN;
+        } else if (xVelocity < 0 && absX > absY) {
+            direction = Direction.RIGHT;
+        } else if (xVelocity > 0 && absX > absY) {
+            direction = Direction.LEFT;
+        } else {
+            direction = Direction.UP;
+        }
     }
 }
